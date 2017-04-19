b525e055c24a3d64d27fd511593853733d37bd78
Alayna Buysse <alaynagrace@comcast.net>
2017-04-19 15:36:38 -0500

    added money gif

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ http.createServer(function(req,res){
   res.writeHead(200); //200 is a safe code
   //write 'account balance: ' and put up a random balance using functions
   res.write('<h1>'+ account.accountBalance() + '</h1><h2>' + account.takeRandom(rand(100,1000000)) + '</h2>');
+  res.write('<img src="https://media.giphy.com/media/ilMRzDNuT1sty/giphy.gif">');
   //end it so it can send the response
   res.end();
 
